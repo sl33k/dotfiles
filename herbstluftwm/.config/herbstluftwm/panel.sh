08ea22af8a299e15f744308a69f4d3e7bb31663f
Malte 's33lk' Bargholz <malte@screenri.de>
2019-01-17 16:13:29 +0530

    move herbstluft to gruvbox

--- herbstluftwm/.config/herbstluftwm/panel.sh
+++ herbstluftwm/.config/herbstluftwm/panel.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 script_path="`dirname \"$0\"`"
-source "$script_path/solarized_rc"
+source "$script_path/colors_rc"
 
 for monitor in "$@"; do
     herbstclient pad $monitor 20
 done
 ~/.config/herbstluftwm/wallpaper.sh & 
 yabar 2>&1 > /dev/null &
-(sleep 3 && stalonetray -bg "#${sol_base03:1}" --dockapp-mode simple --icon-size 20 --geometry "5x1+2250+0" --icon-gravity NE --window-strut right) &
+(sleep 3 && stalonetray -bg "#${base03:1}" --dockapp-mode simple --icon-size 20 --geometry "5x1+2250+0" --icon-gravity NE --window-strut right) &
 herbstclient --wait '^(quit_panel|reload).*'
 
 pkill yabar
